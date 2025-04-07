9c83a91afbdcf4596211ec4ebe3fe79050991983
Iago <iago_galdino@hotmail.com>
2025-04-07 14:34:15 -0300

    config db

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -3,15 +3,21 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// const configDB = {
+//   host: process.env.DB_HOST || '0.0.0.0',
+//   database: process.env.DB_NAME || 'postgres',
+//   user: process.env.DB_USER || 'postgres',
+//   password: process.env.DB_PASSWORD || 'admin',
+//   port: parseInt(process.env.DB_PORT || '5432'),
+//   ssl: {
+//     rejectUnauthorized: false  // Necessário para conexões externas seguras no Render
+//   }
+// };
 const configDB = {
-  host: process.env.DB_HOST || '0.0.0.0',
-  database: process.env.DB_NAME || 'postgres',
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'admin',
-  port: parseInt(process.env.DB_PORT || '5432'),
-  ssl: {
-    rejectUnauthorized: false  // Necessário para conexões externas seguras no Render
-  }
+  connectionString: process.env.INTERNAL_DATABASE_URL,
+  // ssl: {
+  //   rejectUnauthorized: false  // Necessário para conexões externas no Render
+  // }
 };
 
 // console.log(configDB);
